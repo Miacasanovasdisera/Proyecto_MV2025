deff310a62a88fb8edd5d0e36beaf91aa533807a
Mia Casanovas Di Sera <miacasanovasdisera@gmail.com>
2025-09-22 09:30:30 -0300

    correccion de errores en el archivo

--- Test/test3.asm
+++ Test/test3.asm
@@ -1,13 +1,11 @@
-MOV EAX,0X01
+MOV EAX,0x01
 MOV EDX, DS
 LDL ECX, 1
 LDH ECX, 4
-SYS 0X1
-MOV EAX, 0X10
+SYS 0x1
+MOV EAX, 0x10
 MOV EDX, DS 
 LDL ECX, 1
 LDH ECX, 4
-SYS 0X2
+SYS 0x2
 STOP
-//Dado un valor decimal ingresado por teclado, imprimir su valor binario equivalente como una
-secuencia de ceros y unos.
